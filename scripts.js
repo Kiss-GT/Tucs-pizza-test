db9e134c6065d5e7d699a80c33f54337c882cde8
Kiss-GT <kissgergelytamas@gmail.com>
2021-10-03 19:25:32 +0300

    b

--- scripts.js
+++ scripts.js
@@ -23,3 +23,15 @@ const navSlide=()=>{
 
 navSlide();
 
+var lastScrollTop=0;
+navbar=document.getElementById("navbar");
+window.addEventListener("scroll", function(){
+    var scrollTop=window.pageYOffset|| this.document.documentElement.scrollTop;
+    if (scrollTop>lastScrollTop){
+        navbar.style.top="-80px";
+    } else {
+        navbar.style.top="0";
+    }
+    lastScrollTop=scrollTop;
+
+})
